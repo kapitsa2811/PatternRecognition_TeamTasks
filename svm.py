0497be90d0cf564ee8883094b03f6e14cdedd3d0
nela <manu.h@me.com>
2017-03-20 15:21:48 +0100

    created start file

---
+++ svm.py
@@ -0,0 +1,12 @@
+"""
+Authors:
+- Livio Baetscher
+- Manuela Haefliger
+- Marc-Antoine Jacques
+Date created: 20/3/2017
+Date last modified: 20/3/2017
+Python Version: 3.6
+
+Pattern Recognition - Exercise 2a
+"""
+
