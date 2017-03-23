4c031c91d3195fbbbfaba2cfc0ca443f4d71a1ef
manu <manu.h@me.com>
2017-03-23 18:13:02 +0100

    scores for cross validation

--- Exercise_2a/svm.py
+++ Exercise_2a/svm.py
@@ -45,6 +45,9 @@ linear_svc = svm.SVC(kernel='linear', C=1, gamma=0.002)
 linear_svc.fit(trainFeatures, trainLabels)
 predicted_labels_linear = linear_svc.predict(testFeatures)
 accuracy_linear = np.sum(np.equal(predicted_labels_linear, testLabels))/len(testLabels)
+scores = cross_val_score(clf, testFeatures, testLabels, cv=5)
+scores
+
 
 # Example for SVM with RBF kernel
 
