5305b9a2c1ec79cc94977ab82141a571f03f2c71
manu <manu.h@me.com>
2017-03-26 15:43:50 +0200

    completed grid search

--- Exercise_2a/svm.py
+++ Exercise_2a/svm.py
@@ -13,11 +13,20 @@ import csv, time
 import numpy as np
 from sklearn.svm import SVC
 from sklearn import metrics
+from sklearn.model_selection import GridSearchCV
 
+# ---------------------------------------------------------------------------------------------------------------------
+# DATA PREPARATION
+
+print()
+print('>> start data preparation')
+
+# import train data set
 with open('train.csv', 'r') as f:
     reader = csv.reader(f)
     train = list(reader)
 
+# import test data set
 with open('test.csv', 'r') as f:
     reader = csv.reader(f)
     test = list(reader)
@@ -34,59 +43,112 @@ trainFeatures = train[:, 1:]
 testLabels = test[:, 0]
 trainLabels = train[:, 0]
 
+# normalize the labels to be in the range [0,1]
+trainFeatures = trainFeatures/255
+testFeatures = testFeatures/255
+
 # We no longer need the samples with labels and the objects for import
 del train, test, reader, f
 
-print('done importing')
+print('>> done preparing data')
 
 # ---------------------------------------------------------------------------------------------------------------------
+# ONLY USE PART OF THE DATA
 
-# trainFeatures = trainFeatures[:1000, :]
-# trainLabels = trainLabels[:1000]
-# testFeatures = testFeatures[:20, :]
-# testLabels = testLabels[:20]
+trainFeatures = trainFeatures[:10000, :]
+trainLabels = trainLabels[:10000]
+testFeatures = testFeatures[:1000, :]
+testLabels = testLabels[:100]
 
 # ---------------------------------------------------------------------------------------------------------------------
-# linear kernel
+# LINEAR KERNEL
 
-# settings:
-c_lin = 1
+start_lin = time.time()
+
+# number of sets in cross validation:
+cv_num_lin = 5
+
+# parameters to test in grid search:
+params_lin = {"C": [1, 2, 3, 4, 5, 6, 7, 8, 9]}
 
 print()
-print("--- LINEAR KERNEL ---")
+print("---- LINEAR KERNEL ----")
 
-start_svm_lin = time.time()
+# set up svm for parameter optimization
+svm_lin = SVC(kernel='linear', cache_size=4000)
 
-svc_lin = SVC(kernel='linear', C=c_lin)
-svc_lin.fit(trainFeatures, trainLabels)
-pred_labels_lin = svc_lin.predict(testFeatures)
+# grid search
+gs_svm_lin = GridSearchCV(svm_lin, param_grid=params_lin, scoring='accuracy', n_jobs=2, cv=cv_num_lin)
+gs_svm_lin.fit(trainFeatures, trainLabels)
+# note: in the end, the svm is refit with the best estimator and the entire data set
+
+# read tested parameter combinations and their scores
+params_gs_lin = gs_svm_lin.cv_results_['params']
+score_cv_lin = gs_svm_lin.cv_results_['mean_test_score']
+
+# output tested parameters and scores
+print()
+print('C | accuracy')
+print('------------')
+for i in range(len(params_gs_lin)):
+    print('{:1.0f} | {:1.4f}'.format(params_gs_lin[i]['C'], score_cv_lin[i]))
+print()
+
+# predict labels for test set
+pred_labels_lin = gs_svm_lin.predict(testFeatures)
 accuracy_lin = metrics.accuracy_score(pred_labels_lin, testLabels)
 
-end_svm_lin = time.time()
+end_lin = time.time()
 
-print("Accuracy of SVM with linear kernel and optimal parameter C = {:1.5f} on training set is {:1.5f}".format(c_lin, accuracy_lin))
-print('Time elapsed: ' + repr(end_svm_lin-start_svm_lin) + ' s')
+best_C_lin = gs_svm_lin.best_params_['C']
 
+print("Accuracy of SVM with linear kernel and optimal parameter C = {:1.0f} on training set is {:1.4f}".format(best_C_lin, accuracy_lin))
+print('Time elapsed for linear kernel: ' + repr(end_lin-start_lin) + ' s')
+print()
 
 # ---------------------------------------------------------------------------------------------------------------------
-# RBF kernel
+# RBF KERNEL
 
-# settings:
-c_rbf = 3
-gamma_rbf = 0.0005
+start_rbf = time.time()
+
+# number of sets in cross validation:
+cv_num_rbf = 5
+
+# parameters to test in grid search:
+params_rbf = {"C": [1, 2, 3], "gamma": [0.01, 0.001, 0.0001]}
 
 print()
-print("--- RBF KERNEL ---")
+print("---- RBF KERNEL ----")
 
-start_svm_rbf = time.time()
+# set up svm for parameter optimization
+svm_rbf = SVC(kernel='rbf', cache_size=4000)
 
-svc_rbf = SVC(kernel='rbf', C=c_rbf, gamma=gamma_rbf)
-svc_rbf.fit(trainFeatures, trainLabels)
-pred_labels_rbf = svc_rbf.predict(testFeatures)
-accuracy_rbf = metrics.accuracy_score(pred_labels_lin, testLabels)
+# grid search
+gs_svm_rbf = GridSearchCV(svm_rbf, param_grid=params_rbf, scoring='accuracy', n_jobs=2, cv=cv_num_rbf)
+gs_svm_rbf.fit(trainFeatures, trainLabels)
+# note: in the end, the svm is refit with the best estimator and the entire data set
 
-end_svm_rbf = time.time()
+# read tested parameter combinations and their scores
+params_gs_rbf = gs_svm_rbf.cv_results_['params']
+score_cv_rbf = gs_svm_rbf.cv_results_['mean_test_score']
 
-print("Accuracy of SVM with RBF kernel and optimal parameter C = {:1.5f} and gamma = {:1.5f} on training set is {:1.5f}".format(c_rbf, gamma_rbf, accuracy_rbf))
-print('Time elapsed: ' + repr(end_svm_rbf-start_svm_rbf) + ' s')
+# output tested parameters and scores
+print()
+print('C | gamma   | accuracy')
+print('----------------------')
+for i in range(len(params_gs_rbf)):
+    print('{:1.0f} | {:1.5f} | {:1.4f}'.format(params_gs_rbf[i]['C'], params_gs_rbf[i]['gamma'], score_cv_rbf[i]))
+print()
+
+# predict labels for test set
+pred_labels_rbf = gs_svm_rbf.predict(testFeatures)
+accuracy_rbf = metrics.accuracy_score(pred_labels_rbf, testLabels)
+
+end_rbf = time.time()
+
+best_C_rbf = gs_svm_rbf.best_params_['C']
+best_gamma_rbf = gs_svm_rbf.best_params_['gamma']
+
+print("Accuracy of SVM with RBF kernel and optimal parameter C = {:1.0f} and gamma = {:1.5f} on training set is {:1.4f}".format(best_C_rbf, best_gamma_rbf, accuracy_rbf))
+print('Time elapsed for RBF kernel: ' + repr(end_rbf-start_rbf) + ' s')
 print()
