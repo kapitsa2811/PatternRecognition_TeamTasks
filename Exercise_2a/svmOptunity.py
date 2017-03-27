7df8f76b4d10a4dd3d779da3984c05712643a3a8
manu <manu.h@me.com>
2017-03-27 11:36:48 +0200

    comments in svmOptunity, added results from optunity to readme

--- Exercise_2a/svmOptunity.py
+++ Exercise_2a/svmOptunity.py
@@ -8,20 +8,11 @@ Pattern Recognition - Exercise 2a
 
 import csv
 import numpy as np
-<<<<<<< HEAD:Exercise_2a/svm.py
-from sklearn.svm import SVC
-from sklearn import metrics
-from sklearn.model_selection import GridSearchCV
+import sklearn.svm
+import optunity.metrics
 
 # ---------------------------------------------------------------------------------------------------------------------
 # DATA PREPARATION
-
-print()
-print('>> start data preparation')
-=======
-import sklearn.svm
-import optunity.metrics
->>>>>>> manu_optunity:Exercise_2a/svmOptunity.py
 
 # import train data set
 with open('train.csv', 'r') as f:
@@ -52,166 +43,86 @@ testFeatures = testFeatures/255
 # We no longer need the samples with labels and the objects for import
 del train, test, reader, f
 
-<<<<<<< HEAD:Exercise_2a/svm.py
-print('>> done preparing data')
-
-=======
->>>>>>> manu_optunity:Exercise_2a/svmOptunity.py
 # ---------------------------------------------------------------------------------------------------------------------
 # IF YOU WANT TO USE ONLY PART OF THE DATA
 
-<<<<<<< HEAD:Exercise_2a/svm.py
 # trainFeatures = trainFeatures[:1000, :]
 # trainLabels = trainLabels[:1000]
 # testFeatures = testFeatures[:100, :]
 # testLabels = testLabels[:100]
-=======
-#trainFeatures = trainFeatures[:7000, :]
-#trainLabels = trainLabels[:7000]
-#testFeatures = testFeatures[:20, :]
-#testLabels = testLabels[:20]
->>>>>>> manu_optunity:Exercise_2a/svmOptunity.py
 
 # ---------------------------------------------------------------------------------------------------------------------
 # LINEAR KERNEL
-
-<<<<<<< HEAD:Exercise_2a/svm.py
-start_lin = time.time()
 
 # number of sets in cross validation:
 cv_num_lin = 5
 
 # parameters to test in grid search:
-params_lin = {"C": [1, 2, 3, 4, 5, 6, 7, 8, 9]}
+param_C_lin = [0, 5]
+num_params_lin = 6
 
-print()
-print("---- LINEAR KERNEL ----")
-
-# set up svm for parameter optimization
-svm_lin = SVC(kernel='linear', cache_size=4000)
-
-# grid search
-gs_svm_lin = GridSearchCV(svm_lin, param_grid=params_lin, scoring='accuracy', n_jobs=2, cv=cv_num_lin)
-gs_svm_lin.fit(trainFeatures, trainLabels)
-# note: in the end, the svm is refit with the best estimator and the entire data set
-=======
-# Call optunity cross validation on train Features and train labels with 5 iterations.
-@optunity.cross_validated(x=trainFeatures.tolist(), y=trainLabels.tolist(), num_folds=5)
+# Optunity cross validation on training set
+@optunity.cross_validated(x=trainFeatures.tolist(), y=trainLabels.tolist(), num_folds=cv_num_lin)
 def svm_linear_tuned_acc(x_train, y_train, x_test, y_test, C):
     model = sklearn.svm.SVC(kernel='linear', C=C).fit(x_train, y_train)
     y_pred = model.predict(x_test)
     acc = np.sum(np.equal(y_test, y_pred))/len(y_test)
     return acc
-
-
-optimal_linear_pars, info_linear, _ = optunity.maximize(svm_linear_tuned_acc, num_evals=6, C=[0, 5])
-#optimal_linear_pars, info_linear, _ = optunity.maximize(svm_linear_tuned_acc, num_evals=20, C=[0, 5], pmap=optunity.pmap)
+optimal_linear_pars, info_linear, _ = optunity.maximize(svm_linear_tuned_acc, num_evals=num_params_lin, C=param_C_lin)
 
 print()
 print("--- LINEAR KERNEL ---")
 print("Optimal parameters: " + str(optimal_linear_pars))
-print("Accuracy of tuned SVM with linear kernel: %1.3f" % info_linear.optimum)
->>>>>>> manu_optunity:Exercise_2a/svmOptunity.py
+print("Accuracy of tuned SVM with linear kernel: {:1.3f}".format(info_linear.optimum))
 
-# read tested parameter combinations and their scores
-params_gs_lin = gs_svm_lin.cv_results_['params']
-score_cv_lin = gs_svm_lin.cv_results_['mean_test_score']
-
-<<<<<<< HEAD:Exercise_2a/svm.py
 # output tested parameters and scores
-print()
-print('C | accuracy')
-print('------------')
-for i in range(len(params_gs_lin)):
-    print('{:1.0f} | {:1.4f}'.format(params_gs_lin[i]['C'], score_cv_lin[i]))
-print()
-=======
->>>>>>> manu_optunity:Exercise_2a/svmOptunity.py
+df_linear = optunity.call_log2dataframe(info_linear.call_log)
+print(df_linear)
 
-# predict labels for test set
-pred_labels_lin = gs_svm_lin.predict(testFeatures)
-accuracy_lin = metrics.accuracy_score(pred_labels_lin, testLabels)
+# train svm with optimal parameter and calculate accuracy
+svc_best_linear = sklearn.svm.SVC(kernel='linear', C=optimal_linear_pars['C'])
+svc_best_linear.fit(trainFeatures, trainLabels)
+predicted_labels_linear = svc_best_linear.predict(testFeatures)
+accuracy_linear = float(np.sum(np.equal(predicted_labels_linear, testLabels))/len(testLabels))
 
-<<<<<<< HEAD:Exercise_2a/svm.py
-end_lin = time.time()
-
-best_C_lin = gs_svm_lin.best_params_['C']
-=======
 print("Accuracy of SVM with linear kernel and optimal parameter C = {:1.3f} on training set is {:1.3f}".format(optimal_linear_pars['C'], accuracy_linear))
->>>>>>> manu_optunity:Exercise_2a/svmOptunity.py
-
-print("Accuracy of SVM with linear kernel and optimal parameter C = {:1.0f} on training set is {:1.4f}".format(best_C_lin, accuracy_lin))
-print('Time elapsed for linear kernel: ' + repr(end_lin-start_lin) + ' s')
 print()
 
 # ---------------------------------------------------------------------------------------------------------------------
-<<<<<<< HEAD:Exercise_2a/svm.py
-# RBF KERNEL
-=======
 # RBF kernel
-# norm features
-trainFeatures = trainFeatures/255;
-testFeatures = testFeatures/255;
->>>>>>> manu_optunity:Exercise_2a/svmOptunity.py
 
-start_rbf = time.time()
-
-<<<<<<< HEAD:Exercise_2a/svm.py
 # number of sets in cross validation:
 cv_num_rbf = 5
 
 # parameters to test in grid search:
-params_rbf = {"C": [1, 2, 3, 4], "gamma": [0.01, 0.001, 0.0001]}
+param_C_rbf = [1, 5]
+param_gamma_rbf = [0.0006, 0.006]
+num_params_rbf = 6
 
-print()
-print("---- RBF KERNEL ----")
-
-# set up svm for parameter optimization
-svm_rbf = SVC(kernel='rbf', cache_size=4000)
-=======
-
-optimal_rbf_pars, info_rbf, _ = optunity.maximize(svm_rbf_tuned_acc, num_evals=6, C=[1, 5], gamma=[0.0006, 0.006])
-#optimal_rbf_pars, info_rbf, _ = optunity.maximize(svm_rbf_tuned_acc, num_evals=20, C=[2, 4], gamma=[0.0001, 0.01], pmap=optunity.pmap)
+# Optunity cross validation on training set
+@optunity.cross_validated(x=trainFeatures.tolist(), y=trainLabels.tolist(), num_folds=cv_num_rbf)
+def svm_rbf_tuned_acc(x_train, y_train, x_test, y_test, C, gamma):
+    model = sklearn.svm.SVC(kernel='rbf', C=C, gamma=gamma).fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+    acc = np.sum(np.equal(y_test, y_pred))/len(y_test)
+    return acc
+optimal_rbf_pars, info_rbf, _ = optunity.maximize(svm_rbf_tuned_acc, num_evals=num_params_rbf, C=param_C_rbf, gamma=param_gamma_rbf)
 
 print()
 print("--- RBF KERNEL ---")
 print("Optimal parameters: " + str(optimal_rbf_pars))
-print("Accuracy of tuned SVM with RBF kernel: %1.3f" % info_rbf.optimum)
->>>>>>> manu_optunity:Exercise_2a/svmOptunity.py
-
-# grid search
-gs_svm_rbf = GridSearchCV(svm_rbf, param_grid=params_rbf, scoring='accuracy', n_jobs=2, cv=cv_num_rbf)
-gs_svm_rbf.fit(trainFeatures, trainLabels)
-# note: in the end, the svm is refit with the best estimator and the entire data set
-
-# read tested parameter combinations and their scores
-params_gs_rbf = gs_svm_rbf.cv_results_['params']
-score_cv_rbf = gs_svm_rbf.cv_results_['mean_test_score']
+print("Accuracy of tuned SVM with RBF kernel: {:1.3f}".format(info_rbf.optimum))
 
 # output tested parameters and scores
-print()
-print('C | gamma   | accuracy')
-print('----------------------')
-for i in range(len(params_gs_rbf)):
-    print('{:1.0f} | {:1.5f} | {:1.4f}'.format(params_gs_rbf[i]['C'], params_gs_rbf[i]['gamma'], score_cv_rbf[i]))
-print()
 
-<<<<<<< HEAD:Exercise_2a/svm.py
-# predict labels for test set
-pred_labels_rbf = gs_svm_rbf.predict(testFeatures)
-accuracy_rbf = metrics.accuracy_score(pred_labels_rbf, testLabels)
-=======
->>>>>>> manu_optunity:Exercise_2a/svmOptunity.py
+df_rbf = optunity.call_log2dataframe(info_rbf.call_log)
+print(df_rbf)
 
-end_rbf = time.time()
+# train svm with optimal parameter and calculate accuracy
+svc_best_rbf = sklearn.svm.SVC(kernel='rbf', C=optimal_rbf_pars['C'], gamma=optimal_rbf_pars['gamma'])
+svc_best_rbf.fit(trainFeatures, trainLabels)
+predicted_labels_rbf = svc_best_rbf.predict(testFeatures)
+accuracy_rbf = float(np.sum(np.equal(predicted_labels_rbf, testLabels))/len(testLabels))
 
-<<<<<<< HEAD:Exercise_2a/svm.py
-best_C_rbf = gs_svm_rbf.best_params_['C']
-best_gamma_rbf = gs_svm_rbf.best_params_['gamma']
-
-print("Accuracy of SVM with RBF kernel and optimal parameter C = {:1.0f} and gamma = {:1.5f} on training set is {:1.4f}".format(best_C_rbf, best_gamma_rbf, accuracy_rbf))
-print('Time elapsed for RBF kernel: ' + repr(end_rbf-start_rbf) + ' s')
-=======
 print("Accuracy of SVM with RBF kernel and optimal parameter C = {:1.3f} and gamma = {:1.3f} on training set is {:1.3f}".format(optimal_rbf_pars['C'], optimal_rbf_pars['gamma'], accuracy_rbf))
->>>>>>> manu_optunity:Exercise_2a/svmOptunity.py
 print()
--- README.md
+++ README.md
@@ -19,21 +19,24 @@ Team tasks of pattern recognition lecture at Univesity of Fribourg.
 - time
 - numpy 1.12.0
 - scikit-learn 0.18.1
+- optunity 1.1.1 (for installation see [here](http://optunity.readthedocs.io/en/latest/user/installation.html))
 
 ### Data
 - train.csv
 - test.csv
 
 ### Description
-The script `svm.py` builds two SVMs with the provided training set `train.csv` and applies the trained SVMs to classify
-the test set `test.csv`. Two different kernels (linear and RBF) are investigated and the SVM parameters are optimized
-by means of cross-validation (5-fold). The script will output the average accuracy during cross-validation for the
-investigated kernels and all parameter values (C for linear / C and  &gamma; for RBF). The script will also output
+The scripts `svmGridsearch.py` and `svmOptunity.py` build two SVMs with the provided training set `train.csv` each and apply the trained SVMs to classify
+the test set `test.csv`. The two SVMs use different kernels (linear and RBF). Different SVM parameters are optimized
+by means of cross-validation (5-fold). For that, the script `svmGridsearch.py` uses the `GridSearchCV` method from the module `sklearn.model_selection`
+and the script `svmOptunity.py` uses the module `optunity`.
+The scripts will output the average accuracy during cross-validation of the
+investigated kernels and all parameter values (C for linear / C and  &gamma; for RBF). The scripts will also output
 the accuracy on the whole test set with the optimized parameter values.
 
 ### Instructions
-The script can be run in once. For that, the two files `train.csv`and `test.csv`
-have to be in the same folder as the script `svm.py`. Note: the running time is quite
+The scripts can be run in once. For that, the two files `train.csv`and `test.csv`
+have to be in the same folder as the scripts `svm*.py`. Note: the running time is quite
 immense for the entire data set! You might only include a part of the data set by uncommenting the lines
 ```python
 trainFeatures = trainFeatures[:1000, :]
@@ -42,14 +45,15 @@ testFeatures = testFeatures[:100, :]
 testLabels = testLabels[:100]
 ```
 (and maybe adjust the number of samples to include) and/or adjust the k-fold by changing the variables `cv_num_lin` and `cv_num_rbf`
-as well as the tested kernel-parameters `params_lin` and `params_rbf`.
+as well as the tested kernel-parameters `params_lin` and `params_rbf` in `svmGridsearch.py` or `param_C_lin`, `num_params_lin`, `param_C_rbf`
+and `num_params_rbf` in `svmOptunity.py`.
 
 ### Results
 
-From running the script with the entire data set (26'999 samples in `train.csv`,
+From running the scripts with the entire data set (26'999 samples in `train.csv`,
 15'001 samples in `test.csv`).
 
-#### Linear kernel
+#### Linear kernel - gridsearch
 
 Mean accuracy for different parameter C during cross validation:
 
@@ -67,7 +71,22 @@ C | accuracy
 
 Accuracy of SVM with optimal parameter C = 1 on training set is 0.9306.
 
-#### RBF kernel
+#### Linear kernel - optunity
+
+Mean accuracy for different parameter C during cross validation:
+
+C | accuracy
+--- | ---
+0.0391 | 0.9080
+1.2891 | 0.9080
+1.9141 | 0.9080
+2.5391 | 0.9080
+3.7891 | 0.9080
+4.4141 | 0.9080
+
+Accuracy of SVM with optimal parameter C = 4.414 on training set is 0.9080.
+
+#### RBF kernel - gridsearch
 
 Mean accuracy for different parameter C and &gamma; during cross validation:
 
@@ -87,3 +106,19 @@ C | &gamma; | accuracy
 4 | 0.00010 | 0.9101
 
 Accuracy of SVM with optimal parameter C = 4 and &gamma; = 0.01 on training set is 0.9775.
+
+#### RBF kernel - optunity
+
+Mean accuracy for different parameter C and &gamma; during cross validation:
+
+C | gamma | accuracy
+--- | --- | ---
+1.234375 | 0.000959 | 0.926479
+2.234375 | 0.005009 | 0.961628
+2.734375 | 0.002984 | 0.952184
+3.234375 | 0.003659 | 0.958851
+4.234375 | 0.002309 | 0.951517
+4.734375 | 0.005684 | 0.968073
+
+Accuracy of SVM with optimal parameter C = 4.734 and gamma = 0.006 on training set is 0.973.
+
