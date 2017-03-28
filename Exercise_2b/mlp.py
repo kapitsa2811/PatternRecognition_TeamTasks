66a884e5b2c27f8c3c542b32755c7f29b1980cbd
manu <manu.h@me.com>
2017-03-29 00:47:45 +0200

    fixed missing part

--- Exercise_2b/mlp.py
+++ Exercise_2b/mlp.py
@@ -53,10 +53,10 @@ print('>> done preparing data')
 # ---------------------------------------------------------------------------------------------------------------------
 # IF YOU WANT TO USE ONLY PART OF THE DATA
 
-# trainFeatures = trainFeatures[:100, :]
-# trainLabels = trainLabels[:100]
-# testFeatures = testFeatures[:10, :]
-# testLabels = testLabels[:10]
+trainFeatures = trainFeatures[:100, :]
+trainLabels = trainLabels[:100]
+testFeatures = testFeatures[:10, :]
+testLabels = testLabels[:10]
 
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -98,7 +98,7 @@ cv_num = 5  # this will be used for all cross validations performed
 params1 = {'hidden_layer_sizes': np.linspace(10, 100, 10, dtype=int), 'learning_rate_init': np.linspace(0.1, 1, 10)}
 
 # number of times the random initialization should be done:
-num_rand_init = 10
+num_rand_init = 20
 
 # parameters to test in second grid search:
 params2 = {'random_state': np.random.choice(100, size=num_rand_init, replace=False)}
@@ -114,8 +114,8 @@ print()
 print('FIRST CROSS-VALIDATION (FOR NUMBER OF NEURONS AND LEARNING RATE)')
 print('----------------------------------------------------------------')
 
-# the classifier
-mlp1 = MLPClassifier(activation='relu', solver='sgd', learning_rate='constant', max_iter=200,
+# the classifier. we fix a random state for better comparison of the parameters.
+mlp1 = MLPClassifier(activation='relu', solver='sgd', learning_rate='constant', max_iter=200, random_state=42,
                      tol=0.0001, verbose=False)
 
 start1 = time.time()
@@ -167,6 +167,74 @@ print()
 
 # part the indices of the training set into predefined number of sets:
 idx = np.array(np.array_split(np.random.permutation(np.arange(len(trainLabels), dtype=int)), cv_num))
+
+fig, axes = plt.subplots(1, cv_num)
+
+start2 = time.time()
+
+acc_tol = list()
+
+for t in range(len(tols)):
+
+    acc = list()
+    num_iter = list()
+    loss = list()
+
+    axes[t].set_title('tol = ' + repr(tols[t]))
+    axes[t].set_xlabel('number of iterations')
+    axes[t].set_ylabel('loss')
+
+    for i in range(cv_num):
+        x_train = trainFeatures[np.concatenate((np.delete(idx, i, 0))), :]
+        y_train = trainLabels[np.concatenate((np.delete(idx, i, 0)))]
+        x_test = trainFeatures[idx[i], :]
+        y_test = trainLabels[idx[i]]
+        mlp2 = MLPClassifier(hidden_layer_sizes=best_num_neur, activation='relu', solver='sgd',
+                             learning_rate='constant', max_iter=200, random_state=42,
+                             tol=tols[t], learning_rate_init=best_learn_rate, verbose=False)
+        mlp2.fit(x_train, y_train)
+        y_pred = mlp2.predict(x_test)
+        acc.append(metrics.accuracy_score(y_pred, y_test))
+        num_iter.append(mlp2.n_iter_)
+        loss.append(mlp2.loss_)
+        axes[t].plot(mlp2.loss_curve_, label='cv | n_iter = ' + repr(mlp2.n_iter_))
+
+    # do training with entire training set
+    mlp2.fit(trainFeatures, trainLabels)
+    # predict entire test set
+    pred_labels2 = mlp2.predict(testFeatures)
+    acc_train = metrics.accuracy_score(pred_labels2, testLabels)
+    acc.append(acc_train)
+    acc_tol.append(np.mean(acc))
+    axes[t].plot(mlp2.loss_curve_, label='train | n_iter = ' + repr(mlp2.n_iter_))
+    axes[t].legend()
+
+    # output tested parameters and scores
+    print('tolerance = ' + repr(tols[t]))
+    print('------------------')
+    print()
+    print(' ni | loss   | acc')
+    print('---------------------')
+    for i in range(cv_num):
+        print('{:3.0f} | {:.4f} | {:.4f}'.format(num_iter[i], loss[i], acc[i]))
+
+    print()
+    print('Training on entire training set needed {:d} iterations, ended with loss {:.4f} '
+          'and achieved accuracy {:.4f}.'.format(mlp2.n_iter_, mlp2.loss_, acc_train))
+    print()
+    print('The mean accuracy is {:.4f}'.format(acc_tol[t]))
+    print()
+
+end2 = time.time()
+
+best_tol = tols[np.argmax(acc_tol)]
+
+print('ni = number of iterations')
+print('acc = accuracy')
+print()
+print('Best performing tolerance is ' + repr(best_tol) + ' with accuracy {:.4f}.'.format(max(acc_tol)))
+print()
+print('Time elapsed: {:.3f} s'.format(end2-start2))
 
 
 # THIRD CROSS-VALIDATION (FOR NEURON WEIGHTS DUE TO DIFFERENT RANDOM INITIALIZATIONS):
