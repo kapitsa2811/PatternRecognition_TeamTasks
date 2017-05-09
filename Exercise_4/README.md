f2942bcad284fd84311b3e3e8276366ba464b6c9
manu <manu.h@me.com>
2017-05-09 14:44:08 +0200

    added kNN

--- Exercise_4/README.md
+++ Exercise_4/README.md
@@ -46,19 +46,91 @@ In this exercise, the molecules of the validation set are classified using KNN w
 In a first step, the provided dataset is converted into an appropriate data structure. Each molecule is represented as an object of class `Molecule` with the following attributes
 - `id` : the integer part of the molecules filename
 - `atoms` : a (ordered) list with its atoms
-- `length` : number of atoms
+- `atoms_num` : number of atoms
 - `adj_mat` : the adjacency matrix, corresponding to the order of the atoms list
 - `truth` : either 'a' (active) or 'i' (inactive), the ground truth
+- `degree`: number of edges for each atom
 
 Then the molecules are divided into training- and validation-set.
 
-In a next step, the cost matrices between all possible combinations of molecules (one from the training set and one from the validation set) are calcualted.
+In a next step, the cost matrices between all possible combinations of molecules (one from the training set and one from the validation set) are calculated.
 
+In a final step, kNN is performed for different values of k and the accuracy is printed.
 
 ## Instructions
 
+The script `molecules.py` can be run at once and everything will be done.
+
+In the settings part, the deletion/insertion costs for edges and nodes can be defined, as well as a list of parameters k for kNN
+```python
+# =============================================#
+#                   Settings                   #
+# =============================================#
+
+
+# set the costs
+Cn = 1  # cost for node deletion/insertion
+Ce = 1  # cost for edge deletion/insertion
+
+# set k-values for kNN
+K = [1, 3, 5, 10, 15]
+```
+
+If the distance matrix should be saved to a file,
+the part
+```python
+# =============================================#
+#         Save the distance matrix             #
+# =============================================#
+
+
+# save the computed distances
+np.save('dist_1_1', dist)
+
+# note the naming: 'dist_x_y' means the distances
+# have been calculated with Cn = x and Ce = y.
+```
+should be uncommented. Per default this part is commented, hence not active.
+
+If the distance computations should not be done, but read from an input file, then (1) this part
+```python
+# =============================================#
+#             Distance computation             #
+# =============================================#
+
+# compute the distance between all possible combinations of molecule pairs from valid and train (about 8 minutes)
+print()
+print('Computing the distance...')
+start = time.time()
+dist = np.array([calc_dist(mol1, mol2, Cn, Ce) for mol1, mol2 in itertools.product(valid, train)])
+dist = dist.reshape((len(valid), len(train)))
+end = time.time()
+print('Done in {:.2f} seconds!'.format(end-start))
+print()
+```
+should be commented (üer default this part is uncommented, hence active) and (2) this part
+```python
+# =============================================#
+#        Read distance matrix from file        #
+# =============================================#
+
+
+# load the file
+dist = np.load('./distances/dist_1_1.npy')
+
+# note the naming: 'dist_x_y' means the distances
+# been calculated with Cn = x and Ce = y.
+```
+should be uncommented (per default this part is commented, hence not active).
 
 ## Results
+acc | Cn = 1 , Ce = 1
+--- | ---
+k = 1 | 0.992
+k = 3 |0.996
+k = 5 |0.996
+k = 10 |0.992
+k = 15 | 0.988
 
 
 ## Conclusion
--- Exercise_4/molecules.py
+++ Exercise_4/molecules.py
@@ -1,44 +1,62 @@
 """
 Team: Over9000
 
-Python Version: 3.6
+Python Version: 3.6.1
 
-Pattern Recognition - Exercise 4
+Pattern Recognition - Exercise 4 - Molecules
 """
 
 import xml.etree.ElementTree as ET
 import numpy as np
 import itertools
+from scipy.optimize import linear_sum_assignment as lsm
+import time
+
+
+# =============================================#
+#               Class definitions              #
+# =============================================#
 
 
 class Molecule:
     """ A class for representing molecules.
+    
+    Notes
+    -----
+    A molecule is represented as a undirected graph. Each atom is a node and atom-atom-connections are edges.
+    In the following, atom and node is used interchangeably. When we talk about the atom-atom-connections,
+    we always use the term edge.
+    
+    The Molecule objects are created from a .gxl file. The file names are of the form XXX.gxl, where XXX is a
+    positive integer number of length 2 to 5 without leading zeros.
+    
+    To create a molecule, the ground truth has to be given as well.
 
     Parameters
     ----------
     id : str
-        The string contains a human readable number, representing the filename of the molecule.
+        The string contains a human readable number, representing the integer part of the filename of the molecule.
      truth : str
         Human readable string containing the ground truth of the molecules class.
         It's either 'a' for active, or 'i' for inactive.
 
-     Attributes
-     ----------
-     id : int
-        The number contained in the molecules filename, extracted from parameter 'id'.
-     atoms : list
-        A list of strings, each string represents an atom with its chemical symbol.
-     length : int
-        The number of atoms, i.e. the length of tha atoms list.
-     degree : ndarray
-        1D array of length 'length', containing positive integer values, representing the degree
-        of each atom (from list 'atoms'), i.e. number of edges connected to the node (atom).
-     adj_mat : ndarray
-        2D array containing the covalence bonds between the atoms stored in the atoms list.
-        0 means no bond, 1 means single linkage and 2 means double linkage.
-     truth : str
-        Human readable string containing the ground truth of the molecules class.
-        Either 'a' for active or 'i' for inactive.
+    Attributes
+    ----------
+    id : int
+        The integer part of the molecules filename. It's the parameter 'id' converted to int.
+    atoms : list
+        A list of strings, each string represents an atom of the molecule with its chemical symbol, e.g. 'P'.
+    atom_num : int
+        The number of atoms in the molecule, i.e. the length of the list 'atoms'.
+    adj_mat : ndarray
+        2D array containing information about the edges between the atoms.
+        0 means no edge, 1 means edge.
+    degree : ndarray
+        1D array of length 'atom_num', containing positive integer values, representing the degree
+        of each atom (from list 'atoms'), i.e. the number of edges connected to the atom.
+        E.g. the i-th atom of list 'atoms' has degree 'degree[i]'.
+    truth : int
+        Containing the ground truth of the molecules class. 1 means active, 0 inactive and -1 is undefined.
 
     """
 
@@ -46,10 +64,15 @@ class Molecule:
         filename = './data/gxl/' + id + '.gxl'
         self.id = int(id)
         self.atoms = get_atoms(filename)
-        self.length = len(self.atoms)
-        self.adj_mat = get_adj_mat(filename, self.length)
-        self.truth = truth
+        self.atom_num = len(self.atoms)
+        self.adj_mat = get_adj_mat(filename, self.atom_num)
+        self.truth = set_truth(truth)
         self.degree = np.sum(self.adj_mat, axis=0)
+
+
+# =============================================#
+#             Functions definitions            #
+# =============================================#
 
 
 def get_atoms(filename):
@@ -69,10 +92,19 @@ def get_adj_mat(filename, n):
     for edge in tree.findall(".//edge"):
         start = int(edge.get('from')[1:])
         end = int(edge.get('to')[1:])
-        val = int(edge[0][0].text)
-        adj_mat[start-1, end-1] = val
-        adj_mat[end-1, start-1] = val
+        adj_mat[start-1, end-1] = 1
+        adj_mat[end-1, start-1] = 1
     return adj_mat
+
+
+def set_truth(truth):
+    """ Return the truth value as number."""
+    if truth == 'a':
+        return 1
+    elif truth == 'i':
+        return 0
+    else:
+        return -1
 
 
 def load_molecules(name):
@@ -81,42 +113,193 @@ def load_molecules(name):
     with open('./data/' + name + '.txt') as f:
         for line in f:
             id, truth = str.split(line, " ")
+            truth = truth.rstrip()
             mols.append(Molecule(id, truth))
     return mols
 
-# Get the training- and validation sets
 
-train = load_molecules('train')
-valid = load_molecules('valid')
-
-def calc_cost_matrix(mol1, mol2):
-    """ Returns the cost matrix for two given molecules."""
+def calc_dist(mol1, mol2, Cn, Ce):
+    """ Returns the (approximate) graph edit distance of two given molecules using bipartite graph matching.
+    
+    Note
+    ----
+    For further elaborations see 'pr-lecture10.pdf' slides 14-21 and 'pr-lecture9.pdf' slide 36.
+    
+    The (approximate) GED is calculated by solving the assignment problem with the cost matrix C, using
+    the Hungarian algorithm (from scipy.optimize).
+    
+    Let n be the number of atoms of molecule 1 and m the number of atoms of molecule 2.
+    
+    The (n+m) x (n+m) cost matrix C has four parts:
+    1. Upper left: substitutions (nodes plus adjacent edges)  ->  c_i,j for i in 1..n and j in 1..m
+    2. Upper right: deletions (nodes plus adjacent edges)  ->  c_i,eps for i in 1..n on the diagonal, rest are 99999
+    3. Lower left: insertions (nodes plus adjacent edges)  ->  c_eps,j for j in 1..m on the diagonal, rest are 99999
+    4. Lower right: dummy assignments (eps → eps)  ->  0
+    
+    Assume u_i to be a node from molecule 1 and v_i a node from molecule 2.
+    Let P_i be the set of all adjacent edges to u_i and Q_i the set of all adjacent edges to v_i.
+    
+    Then, the entries (c_i,j / c_i,eps / c_eps,j) of the cost matrix C are:
+    - Deletion costs include the deletion of the node u_i as well as the deletion of all edges in P_i :
+        c_i,eps = c(u_i -> eps) + sum_{p in P_i} c(p -> eps)
+    - Insertion costs include the insertion of the node v_i as well as the insertion of all edges in Q_i :
+        c_eps,j = c(eps -> v_j) + sum_{q in Q_i} c(eps -> q)
+    - Substitution costs include the node substitution (u_i -> v_i) as well as an estimation of the 
+      edge assignment cost C(P_i -> Q_j) :
+        c_i,j = c(u_i -> v_j) + C(P_i -> Q_j)
+    
+    For insertion and deletion cost we consider the Dirac cost function, where we fix two positive numbers Ce and Cn:
+    - node substitution: c(u_i -> v_j) = 2*Cn if symbols are not equal,
+                         c(u_i -> v_j) = 0    otherwise.
+    - node deletion/insertion: c(u_i -> eps) = c(eps -> v_j) = Cn
+    - edge deletion/insertion: c(p -> eps) = c(eps -> q) = Ce
+        
+    The estimation of the edge assignment costs C(P_i -> Q_j) are assumed to be the number of edges that have to be 
+    either deleted or inserted to get from P_i to Q_j. Hence the absolute value of the difference in node number of
+    the two sets: abs(| P_i | - | Q_j |).
+  
+    """
 
     # read how many atoms each molecule has
-    n = mol1.length
-    m = mol2.length
+    n = mol1.atom_num
+    m = mol2.atom_num
 
-    # set the costs
-    Cn = 1  # cost for node deletion/insertion:
-    Ce = 1  # cost for edge deletion/insertion
+    # read how many edges each atom of each molecule has
+    P = mol1.degree
+    Q = mol2.degree
 
-    # initiate cost matrix, filled with zeros
-    cost_mat = np.zeros((n+m, n+m), dtype=float)
+    # ------------------------------------------------------------------------------------------------------------------
+    # COMPUTE THE ESTIMATION OF THE EDGE ASSIGNMENT COSTS C(P_i -> C_j)
 
-    # enter deletion for mol1
-    upper_right = np.diag(Cn + Ce*mol1.degree)
-    upper_right[upper_right == 0] = -1
-    cost_mat[:n, m:] = upper_right
+    edge_ass_cost = np.array([abs(p-q) for p, q in itertools.product(P, Q)])
+    edge_ass_cost = edge_ass_cost.reshape((n, m))
 
-    # enter insertions for mol2
-    lower_left = np.diag(Cn + Ce*mol2.degree)
-    lower_left[lower_left == 0] = -1
-    cost_mat[n:, :m] = lower_left
+    # ------------------------------------------------------------------------------------------------------------------
+    # COMPUTE THE COST MATRIX C
 
-    # enter substitutions
-    mat = np.array([a!=b for a, b in itertools.product(mol1.atoms, mol2.atoms)])
-    mat = mat.reshape((n, m))
-    mat = mat*2*Cn
-    cost_mat[:n, :m] = mat
+    # initiate cost matrix, filled with zeros (since the lower right part is all zeroes anyway)
+    cost_mat = np.zeros((n+m, n+m))
 
-    return cost_mat
+    # compute the upper right part
+    upper_right = np.diag(Cn + Ce * P)  # directly compute the diagonal matrix
+    upper_right[upper_right == 0] = 99999  # replace all non-diagonal elements
+    cost_mat[:n, m:] = upper_right  # insert the part in the cost matrix
+
+    # compute the lower left part
+    lower_left = np.diag(Cn + Ce * Q)  # directly compute the diagonal
+    lower_left[lower_left == 0] = 99999  # replace all non-diagonal elements
+    cost_mat[n:, :m] = lower_left  # insert the part in the cost matrix
+
+    # compute the upper left part
+    upper_left = [a != b for a, b in itertools.product(mol1.atoms, mol2.atoms)]  # check where the symbols are not equal
+    upper_left = np.array(upper_left)  # convert to np-array
+    upper_left = upper_left.reshape((n, m))  # convert this True/False vector to an (nxm) matrix
+    upper_left = upper_left*2*Cn  # convert True to 2*Cn and False to 0
+    upper_left = upper_left + edge_ass_cost  # add the estimation of the edge assignment cost C(P_i -> C_j)
+    cost_mat[:n, :m] = upper_left  # insert the part in the cost matrix
+
+    # ------------------------------------------------------------------------------------------------------------------
+    # COMPUTE THE GRAPH EDIT DISTANCE
+
+    # use hungarian algorithm to find assignment with minimal cost
+    row_ind, col_ind = lsm(cost_mat)
+
+    # compute the cost of this assignment, which is the graph edit distance
+    ged = cost_mat[row_ind, col_ind].sum()
+
+    return ged
+
+
+# =============================================#
+#                   Settings                   #
+# =============================================#
+
+
+# set the costs
+Cn = 1  # cost for node deletion/insertion
+Ce = 1  # cost for edge deletion/insertion
+
+# set k-values for kNN
+K = [1, 3, 5, 10, 15]
+
+
+# =============================================#
+#              Read the molecules              #
+# =============================================#
+
+# get the validation and the training set (this is very fast)
+valid = load_molecules('valid')
+train = load_molecules('train')
+
+
+# =============================================#
+#             Distance computation             #
+# =============================================#
+
+# compute the distance between all possible combinations of molecule pairs from valid and train (about 8 minutes)
+print()
+print('Computing the distance...')
+start = time.time()
+dist = np.array([calc_dist(mol1, mol2, Cn, Ce) for mol1, mol2 in itertools.product(valid, train)])
+dist = dist.reshape((len(valid), len(train)))
+end = time.time()
+print('Done in {:.2f} seconds!'.format(end-start))
+print()
+
+
+# # =============================================#
+# #         Save the distance matrix             #
+# # =============================================#
+#
+#
+# # save the computed distances
+# np.save('./distances/dist_1_1', dist)
+#
+# # note the naming: 'dist_x_y' means the distances
+# # have been calculated with Cn = x and Ce = y.
+
+
+# # =============================================#
+# #        Read distance matrix from file        #
+# # =============================================#
+#
+#
+# # load the file
+# dist = np.load('./distances/dist_1_1.npy')
+#
+# # note the naming: 'dist_x_y' means the distances
+# # been calculated with Cn = x and Ce = y.
+
+
+# =============================================#
+#                      kNN                     #
+# =============================================#
+
+
+print()
+print('Cn = {} and Ce = {}'.format(Cn, Ce))
+
+for k in K:
+
+    # for each validation-molecule, get the k indices of test-molecules with the smallest distance
+    idx = np.argpartition(dist, k)
+    idx = idx[:, 0:k]
+
+    # get the labels of those k nearest neighbors for each valid-molecule
+    neigh = np.array([[train[i].truth for i in row] for row in idx])
+
+    # for each valid-molecule, find the label that occurs the most (unless k=1, then we're already done)
+    if k == 1:
+        pred = np.concatenate(neigh)
+    else:
+        pred = [np.argmax(np.bincount(row)) for row in neigh]
+
+    # get the ground truth of the molecules in valid
+    truth = np.array([valid[i].truth for i in range(len(valid))])
+
+    # compute the accuracy
+    acc = np.sum(np.equal(truth, pred))/len(valid)
+
+    print()
+    print("accuracy for k = {}: {:.3f}".format(k, acc))
+
